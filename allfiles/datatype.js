2b6e27cf106db5d97b8a25b61d808ff1ca5a55ac
KACHHAP LILY <99547353+kachhaplily@users.noreply.github.com>
2023-04-05 12:04:01 +0530

    ❤️ typescript

---
+++ allfiles/datatype.js
@@ -0,0 +1,8 @@
+// number datatype
+var age = 25;
+var salary = 5000.50;
+console.log(age + salary);
+// string data type 
+var myname = "John";
+var message = 'Hello, world!';
+console.log(myname);
