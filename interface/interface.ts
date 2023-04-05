d94a1009cb6e52351f49fd5591966bedb1850b37
KACHHAP LILY <99547353+kachhaplily@users.noreply.github.com>
2023-04-05 13:18:23 +0530

    interface extends another interface

--- interface/interface.ts
+++ interface/interface.ts
@@ -9,4 +9,11 @@ let User :user={name:"lily",age:23,email:"abc@gmai8l.com",id:1}
 console.log(User)
 
 
+interface user2  extends user{
+    standard:string;
+}
+let User2:user2={name:"abc",age:12,email:"abc@gmail.com",id:2,standard:"5th"}
+console.log(User2)
+
+
 // ? after variable make it optional variable age?
