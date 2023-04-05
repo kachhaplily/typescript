12e52901be35e68e7ce762efae62a73cb0758519
KACHHAP LILY <99547353+kachhaplily@users.noreply.github.com>
2023-04-05 11:11:29 +0530

    all typescript

---
+++ allfiles/datatype.ts
@@ -0,0 +1,25 @@
+// number datatype
+
+let age: number = 25;
+let salary: number = 5000.50;
+
+console.log(age+salary)
+
+// string data type 
+let myname: string = "John";
+let message: string = 'Hello, world!';
+console.log(myname);
+
+// boolean 
+
+let isDone: boolean = false;
+let isLoggedIn: boolean = true;
+
+// array 
+let numbers: number[] = [1, 2, 3, 4, 5];
+let names: string[] = ['Alice', 'Bob', 'Charlie'];
+
+ names.map((value)=>{
+    return(
+    console.log(value))
+})
